bd9d2aab097dc903ab6422cea36bccfb4c9801c2
Yonghwi <yonghwi.nam@gmail.com>
2024-08-30 10:36:29 +0900

    implement basic functions

--- Sources/YHLogger/YHLogger.swift
+++ Sources/YHLogger/YHLogger.swift
@@ -1,2 +1,64 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public func YHDebugLog(_ message: Any?,
+                       functionName: StaticString = #function,
+                       fileName: NSString = #file,
+                       lineNumber: Int = #line,
+                       userInfo: [String: Any] = [:]) {
+    let className = fileName.lastPathComponent
+#if DEBUG
+    if message != nil {
+        print(" 🧐 \(className):\(lineNumber) - \(functionName)> \(message!)")
+    } else {
+        print(" 🧐 \(className):\(lineNumber) >")
+    }
+#endif
+}
+
+public func YHInfoLog(_ message: Any?,
+                      functionName: StaticString = #function,
+                      fileName: NSString = #file,
+                      lineNumber: Int = #line,
+                      userInfo: [String: Any] = [:]) {
+    let className = fileName.lastPathComponent
+#if DEBUG
+    if message != nil {
+        print(" ✅ \(className):\(lineNumber) - \(functionName)> \(message!)")
+    } else {
+        print(" ✅ \(className) \(lineNumber)>")
+    }
+#endif
+}
+
+public func YHWarningLog(_ message: Any?,
+                         functionName: StaticString = #function,
+                         fileName: NSString = #file,
+                         lineNumber: Int = #line,
+                         userInfo: [String: Any] = [:]) {
+    let className = fileName.lastPathComponent
+#if DEBUG
+    if message != nil {
+        print(" ⚠️ \(className):\(lineNumber) - \(functionName)> \(message!)")
+    } else {
+        print(" ⚠️ \(className):\(lineNumber)>")
+    }
+#endif
+}
+
+public func YHErrorLog(_ message: Any?,
+                       functionName: StaticString = #function,
+                       fileName: NSString = #file,
+                       lineNumber: Int = #line,
+                       userInfo: [String: Any] = [:]) {
+    let className = fileName.lastPathComponent
+#if DEBUG
+    if message != nil {
+        print(" ❌ \(className):\(lineNumber) - \(functionName)> \(message!)")
+    } else {
+        print(" ❌ \(className):\(lineNumber)>")
+    }
+#endif
+}
