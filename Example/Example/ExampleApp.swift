ef31ddb7205967ed7f68f2ee0f5689bd3818161f
Yonghwi <yonghwi.nam@gmail.com>
2024-08-30 10:06:40 +0900

    add example project

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Yonghwi on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
